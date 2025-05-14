2341d5fd010bd5bcddab43ef1879ab4ce458694b
Dangelo9 <vitordangelo9@gmail.com>
2025-05-14 20:01:21 -0300

    entrega05(back): finaliza backend

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const pedidosRouter = require('./routes/pedidos');
+
+const app = express();
+const port = 5000; // Ou outra porta de sua preferência
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/pedidos', pedidosRouter);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
